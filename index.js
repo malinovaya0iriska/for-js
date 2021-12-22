0691f033389e3daa5c32354ff216ff7ad30b838e
Irina Martsulevich <malinovayairiska@gmail.com>
2021-12-22 11:32:16 +0300

    create common function for add node, refactor adding loader

--- index.js
+++ index.js
@@ -20,90 +20,11 @@ const dataFull = [
     email: "Nikita@garfield.biz",
     body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
   },
-]
+];
 const URL = 'https://jsonplaceholder.typicode.com/comments';
+const tableHeadings = ['id', 'name', 'email', 'letters amount', 'unique letters'];
 
-const countUniqueLetters = (string) => {
-  const set = new Set(string.replace(/\s+/g, '').split(''));
-  return set.size;
-};
+const root = document.querySelector('#root');
 
-
-const countUnrepeatedLetters = (string) => {
-  const lettersArr = string.replace(/\s+/g, '').split('');
-  const frequencyOfLetterOccurrence = 1;
-
-  const occurrenceOfLettersList = lettersArr.reduce((acc, curr) => {
-      acc[curr] = (acc[curr] || 0) + 1;
-      return acc;
-    }, {}
-  );
-  const uniqueLetters = Object.keys(occurrenceOfLettersList);
-
-  return uniqueLetters.filter(key => occurrenceOfLettersList[key] === frequencyOfLetterOccurrence).length
-};
-
-const generateTableHead = (table, data) => {
-  const thead = table.createTHead();
-  const thRow = thead.insertRow();
-
-  data.forEach(title => {
-      const cell = document.createElement('th');
-      cell.textContent = title
-
-      thRow.appendChild(cell);
-    }
-  )
-}
-
-const createCell = (el, data) => {
-  const cell = el.insertCell();
-  cell.textContent = data;
-
-  el.appendChild(cell);
-};
-
-const generateTableBody = (tbody, data) => {
-
-  data.forEach(({id, name, email, body}) => {
-    const row = tbody.insertRow();
-
-    const rowCellsValues = Object.values({
-      id,
-      name,
-      email
-    }).concat(countUniqueLetters(body), countUnrepeatedLetters(body))
-
-    rowCellsValues.forEach(item => createCell(row, item))
-
-  })
-}
-
-const fillTableData = (url, body) => {
-
-  setTimeout(() => fetch(URL)
-    .then(response => response.json())
-    .then(json => generateTableBody(body, json))
-    .then(() => {
-      setVisible('.loading', false);
-      setVisible('tbody', true);
-    }), 3000)
-};
-
-const createTable = (dataTHead) => {
-
-  const root = document.querySelector('#root');
-  const table = document.createElement('table');
-  const tBody = document.createElement('tbody');
-
-  const body = table.appendChild(tBody);
-
-  root.appendChild(table);
-
-  generateTableHead(table, dataTHead);
-  createLoading(root);
-  fillTableData(URL, body);
-};
-
-createTable(['id', 'name', 'email', 'letters amount', 'unique letters'])
+createTable(root, tableHeadings);
 
