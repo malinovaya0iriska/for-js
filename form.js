62e3adfc46db6c2f9aa1588592f833f8b37e935d
Irina Martsulevich <malinovayairiska@gmail.com>
2022-01-10 11:16:11 +0300

    fix request and displaying response

--- form.js
+++ form.js
@@ -1,15 +1,15 @@
 const createForm = (parentEl, fields) => {
   const form = addNode(parentEl, 'form');
-
   form.style.display = 'grid';
   form.style.gap = '15px';
   form.style.maxWidth = '40%';
 
-  fields.forEach(({ labelTitle, id, type }) => {
+  fields.forEach(({ labelTitle, id, type, name }) => {
     const label = addNode(form, 'label', labelTitle);
     const input = addNode(label, 'input');
     input.id = id;
     input.type = type;
+    input.name = name;
     label.setAttribute('for', id);
   });
 
